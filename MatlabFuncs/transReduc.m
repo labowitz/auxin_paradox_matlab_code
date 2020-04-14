1f4b09ea29b88ee9812558f36dc12bac7aff5b92
ym3141 <yitongma7@gmail.com>
2020-04-14 15:09:21 -0700

    Made cooperativities flexible

--- MatlabFuncs/transReduc.m
+++ MatlabFuncs/transReduc.m
@@ -1,4 +1,5 @@
 function reduc = transReduc(param, eta_A, xdata)
     x = param;
-    reduc = ((eta_A.*xdata + 1).^2 + (x(7)./x(1)).^2)./((x(6) + 1).*(eta_A.*xdata +1).^2 + (x(7)./x(1)).^2);
+    reduc = ((eta_A.*xdata + 1).^x(7) + (x(6)./x(1)).^x(7))./((x(5) + 1).*(eta_A.*xdata +1).^x(7) + (x(6)./x(1)).^x(7));
+%     reduc = ((eta_A.*xdata + 1).^2 + (x(7)./x(1)).^2)./((x(6) + 1).*(eta_A.*xdata +1).^2 + (x(7)./x(1)).^2);
 end
--- apopActi.m
+++ apopActi.m
@@ -1,4 +1,5 @@
 function acti = apopActi(param, eta_A, xdata)
     x = param;
-    acti = (x(4)./x(1))./((x(4)./x(1)) + (eta_A.*xdata + 1));
+    acti = (x(3)./x(1)).^x(8)./((x(3)./x(1)).^x(8) + (eta_A.*xdata + 1).^x(8));
+%     acti = (x(4)./x(1))./((x(4)./x(1)) + (eta_A.*xdata + 1));
 end
