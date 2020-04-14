75a3767221f0c7126323fa828a83ebdc3b1a0762
ym3141 <yitongma7@gmail.com>
2020-04-14 15:26:48 -0700

    Change filename & cleanup

--- apopActi.m
+++
@@ -1,5 +0,0 @@
-function acti = apopActi(param, eta_A, xdata)
-    x = param;
-    acti = (x(3)./x(1)).^x(8)./((x(3)./x(1)).^x(8) + (eta_A.*xdata + 1).^x(8));
-%     acti = (x(4)./x(1))./((x(4)./x(1)) + (eta_A.*xdata + 1));
-end
