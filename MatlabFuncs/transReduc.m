b4167281c99b4f103888201d382b721e34f656b9
ym3141 <yitongma7@gmail.com>
2020-04-13 23:26:06 -0700

    regenerated data, add normalization, and changed cooperativity.

--- MatlabFuncs/transReduc.m
+++ MatlabFuncs/transReduc.m
@@ -1,5 +1,4 @@
 function reduc = transReduc(param, xdata)
     x = param;
-%     reduc = ((x(2).*xdata +1).^4 + (x(7)./x(1)).^4)./((x(6) + 1).*(x(2).*xdata +1).^4 + (x(7)./x(1)).^4);
-    reduc = ((x(2).*xdata +1) + (x(7)./x(1)))./((x(6) + 1).*(x(2).*xdata +1) + (x(7)./x(1)));
+    reduc = ((x(2).*xdata +1).^2 + (x(7)./x(1)).^2)./((x(6) + 1).*(x(2).*xdata +1).^2 + (x(7)./x(1)).^2);
 end
