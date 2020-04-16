426d20b52acf1974df4878b4190f8cc58eea6725
ym3141 <yitongma7@gmail.com>
2020-04-15 19:28:10 -0700

    Changed to generalized logistic

--- MatlabFuncs/endLogest.m
+++ MatlabFuncs/endLogest.m
@@ -1,3 +1,4 @@
-function Y = endLogest(r, Y0, K, t)
-    Y = K * Y0 .* exp(r .* t) ./ (K + Y0 .*  exp(r .* t));
+function Y = endLogest(r, Y0, K, t, nu)
+    Q = (K / Y0) ^ nu - 1;
+    Y = K ./ (1 + Q .* exp(-r * nu .* t)) .^ (1/nu);
 end
