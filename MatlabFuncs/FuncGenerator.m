dd0cdc1aae95c8942effb0a86bcab21f6741495a
ym3141 <yitongma7@gmail.com>
2020-04-16 17:19:09 -0700

    Added FuncGenerator to generate all rate functions, so it can be changed at one place.

---
+++ MatlabFuncs/FuncGenerator.m
@@ -0,0 +1,10 @@
+eta_A = 1/0.76;
+
+rate_func2 = @(x,xdata)([
+    ((x(1) + x(4)) .* transReduc(x, eta_A, xdata)) - x(4);
+    x(1)- x(2) .* apopActi(x, eta_A, xdata)
+    ]);
+
+rate_comb = @(x, xdata)(sum(rate_func2(x, xdata), 1) - x(1));
+
+rate_func3 = @(x, xdata)(cat(1, rate_func2(x, xdata), rate_comb(x, xdata)));
