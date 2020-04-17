0f77848e55e2414d889bda622c6354c2cce7b7e1
ym3141 <yitongma7@gmail.com>
2020-04-16 20:16:03 -0700

    Some cleanup

--- MatlabFuncs/FuncGenerator.m
+++ MatlabFuncs/FuncGenerator.m
@@ -2,7 +2,7 @@ eta_A = 1/0.76;
 
 rate_func2 = @(x,xdata)([
     ((x(1) + x(4)) .* transReduc(x, eta_A, xdata)) - x(4);
-    x(1)- x(2) .* apopActi(x, eta_A, xdata)
+    x(1) - x(2) .* apopActi(x, eta_A, xdata)
     ]);
 
 rate_comb = @(x, xdata)(sum(rate_func2(x, xdata), 1) - x(1));
